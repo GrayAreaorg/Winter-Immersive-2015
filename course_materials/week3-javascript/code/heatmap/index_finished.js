3ecab29f6e7dcf99b43de9613c221fb49d0cedca
Noura <culurienneldoreth@gmail.com>
2015-01-27 17:23:43 -0800

    updated the heatmap example to just use jquery for the get request because it is WAY simpler

--- course_materials/week3-javascript/code/heatmap/index_finished.js
+++ course_materials/week3-javascript/code/heatmap/index_finished.js
@@ -8,46 +8,39 @@ window.onload = function() {
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    var request = new XMLHttpRequest();
-    request.open('GET', 'human-waste-data.json', true);
-    request.onload = function() {
-        console.log('request onload', request);
-        var data = JSON.parse(request.responseText);
-        console.log('data', data);
+    $.ajax({
+        type: 'GET',
+        url: 'human-waste-data.json',
+        success: function(data) {
+            console.log('data', data);
 
-        var print_data = document.getElementById('print-data');
-        var data_display = '';
-        _.each(data, function(d) {
-            data_display += d[13];
-            data_display += '<br><br>';
-        });
-        print_data.innerHTML = data_display;
+            var print_data = document.getElementById('print-data');
+            var data_display = '';
+            _.each(data, function(d) {
+                data_display += d[13];
+                data_display += '<br><br>';
+            });
+            print_data.innerHTML = data_display;
 
-        // parsing poop data
-        var coordinates = _.map(data, function(d) {
-            return [d[21][1], d[21][2]];
-        });
+            // parsing poop data
+            var coordinates = _.map(data, function(d) {
+                return [d[21][1], d[21][2]];
+            });
 
-        // parsing sfpd data
-        /*
-        var coordinates = _.map(data.data, function(d) {
-            return [d[18], d[17]];
-        });
-        */
+            // parsing sfpd data
+            /*
+            var coordinates = _.map(data.data, function(d) {
+                return [d[18], d[17]];
+            });
+            */
+            // convert to Google formatted LatLng objects
+            var google_lat_longs = _.map(coordinates, function(c) {
+                return new google.maps.LatLng(c[0], c[1]);
+            });
 
-        // convert to Google formatted LatLng objects
-        var google_lat_longs = _.map(coordinates, function(c) {
-            return new google.maps.LatLng(c[0], c[1]);
-        });
-
-        var heatmap = make_heat_map(google_lat_longs);
-        //var markers = make_markers(google_lat_longs);
-    };
-    request.onerror = function() {
-        console.log('request onerror', request);
-        // There was a connection error of some sort
-    };
-    request.send();
+            var heatmap = make_heat_map(google_lat_longs);
+        }
+    });
 
     var make_heat_map = function(google_lat_longs) {
         var heat_array = new google.maps.MVCArray(google_lat_longs);
@@ -61,14 +54,5 @@ window.onload = function() {
         return heatmap;
     };
 
-    var make_markers = function(google_lat_longs) {
-        return _.each(google_lat_longs, function(c) {
-            return new google.maps.Marker({
-                map: map,
-                position: c,
-                icon: 'poo.png',
-            });
-        });
-    };
 };
 
